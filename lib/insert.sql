33062248b95575531c5f8b79968f0f0184c6c3ad
Randel Jiao <randeljiao@gmail.com>
2020-09-12 00:16:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,5 +6,15 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Mr. Chocolate", 11, 3.4, 3);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 11, 3.4, 3);
+    
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 11, 3.4, 3);
+    
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 11, 3.4, 3);
+    
     
     
