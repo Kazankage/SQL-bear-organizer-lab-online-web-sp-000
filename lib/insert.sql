8e8c7ddb0626a3a329344173c8dad0f5ee71b937
Randel Jiao <randeljiao@gmail.com>
2020-09-12 00:22:34 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,22 +4,22 @@ INSERT INTO bears (name, age, gender, color, temperament, alive)
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Rowdy",  11, "male", "black", "angry", true);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears 
     VALUES ("Tabitha",  11, "male", "black", "angry", true);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears 
     VALUES ("Sergeant Brown",  11, "male", "black", "angry", true);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears 
     VALUES ("Melissa",  11, "male", "black", "angry", true);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears
     VALUES ("Grinch", 11, "male", "black", "angry", true);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears
     VALUES ("Wendy", 11, "male", "black", "angry", true);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears 
     VALUES (NULL, 11, "male", "black", "angry", true);
     
     
