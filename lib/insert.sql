f73697d472b98a2aa049d1e37ce724c66ac65df2
Randel Jiao <randeljiao@gmail.com>
2020-09-12 00:18:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Mr. Chocolate", 11, 3.4, 3);
     
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 11, 3.4, 3);
-    
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 11, 3.4, 3);
-
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", 11, 3.4, 3);
-    
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 11, 3.4, 3);
-    
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 11, 3.4, 3); 
-    
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 11, 3.4, 3);
-    
-    
-    
